3598f6cb34b62caa64c6cb3f9998462b862688bb
Mauricio Luan <mauricioluan2023@gmail.com>
2025-07-03 08:55:54 -0300

    refactor(api):ajuste na service de venda para recebimento do payload, já aprovado pela Payer

--- src/services/vendaService.js
+++ src/services/vendaService.js
@@ -1,16 +1,4 @@
 const prisma = require('../prisma/client');
-/*
-cara, essa aqui ta hard de fazer, mas tamo indo kkkkkkkkkkk
-o payload virá assim:
-{
-  "operadorId": 1,
-  "metodoPagamento": "Cartão de Crédito",
-  "produtos": [
-    { "produtoId": 5, "quantidade": 2, "valorUnitario": "10.50" },
-    { "produtoId": 8, "quantidade": 1, "valorUnitario": "25.00" }
-  ]
-}
-*/
 
 const criarVenda = async (dadosDoCarrinho) => {
   try {
@@ -22,7 +10,7 @@ const criarVenda = async (dadosDoCarrinho) => {
       throw new Error('Corpo da requisição não é válido');
     }
     return await prisma.$transaction(async (tx) => {
-      const { operadorId, metodoPagamento, produtos } = dadosDoCarrinho;
+      const { operadorId, paymentType, produtos } = dadosDoCarrinho;
 
       // pra eu saber o valor total de todos os produtos do carrinho / requisicao
       const valorTotal = produtos.reduce((valorAnterior, produto) => {
@@ -34,17 +22,12 @@ const criarVenda = async (dadosDoCarrinho) => {
       const novaVenda = await tx.venda.create({
         data: {
           operadorId: operadorId,
-          metodoPagamento: metodoPagamento,
+          metodoPagamento: paymentType,
           valorTotal: valorTotal,
         },
       });
+      console.log('DADOS SALVOS NO DB ->', novaVenda);
 
-      /*
-      esse blco é pra salvar cada produto na entity ProdutosVenda
-      exemplo de como deve vir minha requisição do front
-      { "produtoId": 5, "quantidade": 2, "valorUnitario": "10.50" },
-      { "produtoId": 8, "quantidade": 1, "valorUnitario": "25.00" }
-       */
       const vendaId = novaVenda.id;
       for (const produto of produtos) {
         const { produtoId, quantidade, valorUnitario } = produto;
